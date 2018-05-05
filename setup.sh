5d516a1fa405901e78728c4279a06a1f87841203
gengze <gengze@meituan.com>
2018-05-05 10:36:44 +0800

    open baiduinput folder

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # homebrew
 ./brew.sh
+# don't know how to install '*.app' through command lint
+# open baiduinput folder for now
+open /usr/local/Caskroom/baiduinput/latest
 # install&config karabiner-elements
 ./karabiner.sh
 # install&config oh-my-zsh
