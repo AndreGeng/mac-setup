5aa496a99c383185d1208c06b2a0426c55d95375
gengze <gengze@meituan.com>
2018-05-05 09:35:50 +0800

    add mac-config

---
+++ mac-config.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/Docuemnts/unremitting && cd $_
+git clone https://github.com/AndreGeng/MacConfig.git
+npm run sync
--- setup.sh
+++ setup.sh
@@ -4,15 +4,11 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # homebrew
 ./brew.sh
-# install brew cask downloaded karabiner pkg
+# install&config karabiner-elements
 ./karabiner.sh
-# install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+# install&config oh-my-zsh
+./oh-my-zsh.sh
 # install node with nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install --lts
-nvm alias default lts/*
+./nodejs.sh
+# sync macconfig
+./mac-config.sh
