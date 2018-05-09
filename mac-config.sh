336c1df5b399401638580a94aba42052a95612f9
andregeng <andregeng2011@gmail.com>
2018-05-10 07:46:00 +0800

    bug fix

--- mac-config.sh
+++ mac-config.sh
@@ -1,6 +1,6 @@
 currentDir="$PWD"
 mkdir -p ~/Documents/unremitting && cd $_
-if [ ! -d "~/Documents/unremitting/MacConfig/.git" ]; then
+if [ ! -d ~/Documents/unremitting/MacConfig/.git ]; then
 	git clone https://github.com/AndreGeng/MacConfig.git
 	cd MacConfig
 else
@@ -8,4 +8,4 @@ else
 	git pull
 fi
 ./sync.sh
-cd currentDir
+cd $currentDir
