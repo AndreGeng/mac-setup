169533ac6b15fa89bcd7d193c0e853b499ac251f
gengze <gengze@meituan.com>
2018-05-10 08:20:25 +0800

    add readme.md

---
+++ README.md
@@ -0,0 +1,10 @@
+目前仍需手动配置项
+1. key repeat rate
+  System Preference -> Keyboard -> Keyboard tab
+  两项都调至fast
+2. 使用tab去控制dialog上的button
+  System Preference -> Keyboard -> Shortcuts -> Full Keyboard Access, 打开All controls
+
+todo:
+  hammerspoon
+    cmd+space切换输入法
--- vim.sh
+++ vim.sh
@@ -8,10 +8,8 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 curl -L https://gist.github.com/AndreGeng/5bcf9381cb4080deae401941b96f145e/raw > ~/.vimrc
 # install python3 and neovim python3 provider
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | zsh
-source ~/.zshrc
 pyenv install 2.7.11
 pyenv install 3.6.4
-source ~/.zshrc
 pyenv virtualenv 2.7.11 neovim2
 pyenv virtualenv 3.6.4 neovim3
 
@@ -22,7 +20,3 @@ pyenv which python  # Note the path
 pyenv activate neovim3
 pip install neovim
 pyenv which python  # Note the path
-
-let g:python_host_prog = '/full/path/to/neovim2/bin/python'
-let g:python3_host_prog = '/full/path/to/neovim3/bin/python'
-
