87468c711151c1c60400aca50ed980441d271c2f
gengze <gengze@meituan.com>
2018-05-10 20:48:45 +0800

    add some npm package

--- nodejs.sh
+++ nodejs.sh
@@ -3,3 +3,4 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 source ~/.zshrc
 nvm install --lts
 nvm alias default lts/*
+npm install -g js-beautify // vim autoformat json
