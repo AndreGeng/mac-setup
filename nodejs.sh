494843c1dd14502f4e4872fe8ab51339dbfa2bfb
Andre Geng <andregeng@admindeMacBook-Pro.local>
2018-08-28 15:11:03 +0800

    add typescript global package

--- nodejs.sh
+++ nodejs.sh
@@ -4,4 +4,5 @@ source ~/.zshrc
 nvm install --lts
 nvm alias default lts/*
 npm install -g js-beautify // vim autoformat json
+npm install -g typescript
 sudo ln -s "$(which node)" /usr/local/bin/node
