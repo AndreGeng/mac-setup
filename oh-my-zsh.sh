fc6dd6047816434fdca5b3c32d0b760985bd0027
Andre Geng <andregeng2011@gmail.com>
2018-11-08 15:11:24 +0800

    change oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -3,6 +3,10 @@ for f in $(dirname "$0")/utils/*.sh; do
     source $f
 done
 
+# download .zshrc
+log 'downloading .zshrc' $Green
+curl -o ~/.zshrc https://raw.githubusercontent.com/AndreGeng/MacConfig/master/.zshrc
+
 brewInstallIfNotExists zsh
 brewInstallIfNotExists zsh-completions
 brewInstallIfNotExists autojump
@@ -13,8 +17,3 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # // 安装zsh-syntax-highlighting插件
 log 'installing zsh-syntax-highlighting' $Green
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# download .zshrc
-log 'downloading .zshrc' $Green
-curl -o ~/.zshrc https://raw.githubusercontent.com/AndreGeng/MacConfig/master/.zshrc
-source ~/.zshrc
