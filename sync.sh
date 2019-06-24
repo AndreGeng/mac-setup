118870db38954df4a3fa0ecff79e769073ef4f8b
shanqiu <gengze@innotechx.com>
2019-06-24 17:17:46 +0800

    bug fix

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ done
 
 # install realpath util
 brewInstallIfNotExists coreutils
-
+mkdir -p ~/.config
 rm -rf ~/.config/alacritty
 rm -rf ~/.config/karabiner
 rm -rf ~/.config/nvim
@@ -14,12 +14,13 @@ rm -rf ~/.hammerspoon
 rm -f ~/.zshrc
 rm -f ~/.tmux.conf
 rm -f ~/.agignore
+log $(realpath $(dirname "$0"))
 mkdir -p ~/fzf-addons && ln -sf $(realpath $(dirname "$0"))/mac-config/fzf-git/fzf-git.sh ~/fzf-addons/
-ln -s $(realpath $(dirname "$0"))/mac-config/alacritty ~/.config
-ln -s $(realpath $(dirname "$0"))/mac-config/karabiner ~/.config
-ln -s $(realpath $(dirname "$0"))/mac-config/nvim ~/.config
-ln -s $(realpath $(dirname "$0"))/mac-config/tmuxinator ~/.config
-ln -s $(realpath $(dirname "$0"))/mac-config/.hammerspoon ~
-ln -s $(realpath $(dirname "$0"))/mac-config/.zshrc ~
-ln -s $(realpath $(dirname "$0"))/mac-config/.tmux.conf ~
-ln -s $(realpath $(dirname "$0"))/mac-config/.agignore ~
+ln -s $(realpath $(dirname "$0"))/mac-config/alacritty ~/.config/
+ln -s $(realpath $(dirname "$0"))/mac-config/karabiner ~/.config/
+ln -s $(realpath $(dirname "$0"))/mac-config/nvim ~/.config/
+ln -s $(realpath $(dirname "$0"))/mac-config/tmuxinator ~/.config/
+ln -s $(realpath $(dirname "$0"))/mac-config/.hammerspoon ~/
+ln -s $(realpath $(dirname "$0"))/mac-config/.zshrc ~/
+ln -s $(realpath $(dirname "$0"))/mac-config/.tmux.conf ~/
+ln -s $(realpath $(dirname "$0"))/mac-config/.agignore ~/
