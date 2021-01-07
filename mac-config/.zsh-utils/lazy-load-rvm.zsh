31f7cbaaa1ddec62baf620f2937c70ac013c70aa
山丘 <gengze@innotechx.com>
2021-01-07 21:50:56 +0800

    feat: lazy load rvm

---
+++ mac-config/.zsh-utils/lazy-load-rvm.zsh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+RVM_DIR="$HOME/.rvm"
+
+if [ -d $RVM_DIR ]; then
+  RVM_GLOBALS=(`find ./.rvm/{bin,rubies/**/bin}/* -type l -o -type f -print0 | xargs -0 basename | sort | uniq`)
+fi
+
+load_rvm () {
+  # Unset placeholder functions
+  for cmd in "${RVM_GLOBALS[@]}"; do unset -f ${cmd} &>/dev/null; done
+
+  # Load RVM
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+  export PATH="$PATH:$HOME/.rvm/bin"
+
+  # Do not reload rvm again
+  export RVM_LOADED=1
+}
+
+for cmd in "${RVM_GLOBALS[@]}"; do
+  if ! which ${cmd} &>/dev/null; then
+    eval "${cmd}() { unset -f ${cmd} &>/dev/null; [ -z \${RVM_LOADED+x} ] && load_rvm; ${cmd} \$@; }"
+  fi
+done
