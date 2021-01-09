76d69e7d1579d8c1783a29ec8658ea12ab275735
Andre Geng <andregeng@Andres-MacBook-Air.local>
2021-01-09 10:42:39 +0800

    feat: 修改homebrew安装脚本

--- ss.sh
+++ ss.sh
@@ -2,5 +2,5 @@
 base_dir=$(dirname "$0")
 source $base_dir/utils/2.log.sh
 source $base_dir/utils/install_homebrew_if_needed.sh
-brew cask install shadowsocksx-ng --force
+brew install --force --cask shadowsocksx-ng
 
--- utils/install_homebrew_if_needed.sh
+++ utils/install_homebrew_if_needed.sh
@@ -6,6 +6,6 @@ if exists brew; then
   log "homebrew already exists!" $GREEN
 else
   log "Installing Homebrew Now"
-  /bin/bash -c "$(curl -x http://localhost:1087 -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 fi
 export HOMEBREW_FORCE_BREWED_CURL=1
